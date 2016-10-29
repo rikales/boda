d26bffc3889c9351ed4f31e9da439e42f3ea13bc
adria.rica <adriarica@gmail.com>
2016-10-29 16:18:16 +0200

    primer commit

---
+++ src/main/java/com/adria/sandra/BodaApplication.java
@@ -0,0 +1,12 @@
+package com.adria.sandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodaApplication.class, args);
+	}
+}
